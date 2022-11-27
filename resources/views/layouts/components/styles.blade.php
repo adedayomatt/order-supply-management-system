e78856ec7bddf6ffc758a787e9ff5cb4b3adaaf3
Adedayo Matthew <32306570+adedayomatt@users.noreply.github.com>
2022-11-27 15:59:30 +0100

    Adjustments

--- resources/views/layouts/components/styles.blade.php
+++ resources/views/layouts/components/styles.blade.php
@@ -5,35 +5,44 @@
 <!-- Extra CSS -->
 <style>
     body{
-        background-color: #f7f7f7;
+        background-color: #e3e3e3;
         /* background-image: url({{asset('storage/images/bg/sand-truck.jpeg')}}); */
         background-repeat: no-repeat;
         background-position: center;
         background-size: cover;
         background-attachment: fixed;
     }
+    main{
+        padding-top: 120px
+    }
     body::after{
 
     }
-.scrollable{
-    max-height: 80vh;
-    overflow: auto;
-}
-.hidden-action{
-    display: none;
-}
-.has-hidden-action:hover .hidden-action{
-    display: block
-}
-.content-box{
-    background-color: rgba(255,255,255,.8);
-    padding: 10px;
-    box-shadow: 0px 10px 10px rgba(0,0,0,.3);
-    border-radius: 4px;
-    margin: 10px 0;
-}
-.avatar-sm{
-    width: 100px;
-}
+    .scrollable{
+        max-height: 80vh;
+        overflow: auto;
+    }
+    .hidden-action{
+        display: none;
+    }
+    .has-hidden-action:hover .hidden-action{
+        display: block
+    }
+    .content-box{
+        background-color: rgba(255,255,255,.8);
+        padding: 10px;
+        box-shadow: 0px 10px 10px rgba(0,0,0,.3);
+        border-radius: 4px;
+        margin: 10px 0;
+    }
+    .avatar-sm{
+        width: 100px;
+    }
+/* sm screen and above*/
+    @media (min-width: 576px){
+        main{
+            padding-top: 80px
+        }
+    }
 </style>
 @yield('styles')
